93bb3d0a77b131d0c3f50e6a346f569b2128f676
Sana Parveen <misana348@gmail.com>
2024-05-19 15:45:43 +0530

    "Added new dependencies, updated package-lock.json and package.json, and modified src/app/(auth)/sign-in/page.tsx to use react-hook-form, zod, and usehooks-ts"

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,21 @@
-import React from 'react'
+"use client"
 
-function page() {
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link";
+import { useState } from "react";
+import { useDebounceValue } from 'usehooks-ts';
+
+const page = () => {
+  const [username, setUsername] = useState('')
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+const debouncedUsername = useDebounceValue(username, 300)
+
   return (
-    <div>
-      Page
-    </div>
+    <div>page</div>
   )
 }
-
-export default page
