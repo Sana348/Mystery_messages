cbd56be6745e71124b38816fd1abfa319af62f72
Sana Parveen <misana348@gmail.com>
2024-05-27 12:06:50 +0530

    "Removed auth layout files, updated app layout, and deleted API route for deleting messages"

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,14 +1,32 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+// import type { Metadata } from 'next';
+// import { Inter } from 'next/font/google';
+
+// import Navbar from '@/components/Navbar';
+
+// const inter = Inter({ subsets: ['latin'] });
+
+// export const metadata: Metadata = {
+//   title: 'True Feedback',
+//   description: 'Real feedback from real people.',
+// };
+
+// interface RootLayoutProps {
+//   children: React.ReactNode;
+// }
+
+// export default async function RootLayout({ children }: RootLayoutProps) {
+//   return (
+//     <html lang="en" >
+//       <body className={inter.className}>
+//         <Navbar />
+//         {children}
+//       </body>
+
+//     </html>
+//   );
+// }
 
 import Navbar from '@/components/Navbar';
-
-const inter = Inter({ subsets: ['latin'] });
-
-export const metadata: Metadata = {
-  title: 'True Feedback',
-  description: 'Real feedback from real people.',
-};
 
 interface RootLayoutProps {
   children: React.ReactNode;
@@ -16,16 +34,9 @@ interface RootLayoutProps {
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en" >
-
-      <body className={inter.className}>
-
-        <Navbar />
-        {children}
-
-
-      </body>
-
-    </html>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {children}
+    </div>
   );
 }
